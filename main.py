ed2569eadbc00134ba73174244db7bdc5af89e66
Jayant Singhal <jayantsinghal992@gmail.com>
2025-07-27 07:02:58 +0530

    --

--- main.py
+++ main.py
@@ -19,8 +19,23 @@ def intro():
 
 @app.post('/get-ticker/{ticker}')
 def get_ticker(ticker: str = Path(..., description="This is just for an example")):
-    return {"Hello":"Testing Purpose"}
-    
+    try:
+        pipeline = create_pipeline(ticker=ticker)
+        pipeline.fit(pd.DataFrame())
+        joblib.dump(pipeline, 'stock_prediction_pipeline.joblib')
+        return {'message':f'Pipeline trained and saved for ticker {ticker}'}
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail=str(e))
+
+@app.get('/get-ticker/prediction')
+def ticker_prediction():
+    try:
+        pipeline = joblib.load('stock_prediction_pipeline.joblib')
+        forecast_df = pipeline.predict()
+        forecast_json = forecast_df.to_json(orient='records')  
+        return JSONResponse(content={'forecast':forecast_json})  
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))        
     
 
     
